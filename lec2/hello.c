4e6f1d3df2f086053c04d3ce0fe104962850ae62
dipak saket <saketdeepak009@gmail.com>
2025-09-18 23:06:52 +0530

    firstcommit

---
+++ lec2/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // User se input lena
+    printf("Enter the number num1: ");
+    scanf("%d", &num1);
+
+    printf("Enter the number num2: ");
+    scanf("%d", &num2);
+
+    // Dono ka sum
+    sum = num1 + num2;
+
+    // Result dikhana
+    printf("the value is sum: %d\n", sum);
+
+    return 0;
+}
